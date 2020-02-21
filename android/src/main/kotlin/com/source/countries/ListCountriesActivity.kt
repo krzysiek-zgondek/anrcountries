e602249aa7763cf4f844e432bcbc220ae0df0d2b
krzysiek zgondek <krzysiek.zgondek@gmail.com>
2020-02-21 03:04:11 +0100

    #attached missed file

---
+++ android/src/main/kotlin/com/source/countries/ListCountriesActivity.kt
@@ -0,0 +1,11 @@
+package com.source.countries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListCountriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_countries)
+    }
+}
