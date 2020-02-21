755c4ec037f1965ee4c9afd2757c2f40c2e5e090
krzysiek zgondek <krzysiek.zgondek@gmail.com>
2020-02-21 08:25:35 +0100

    #added lifecycle viewmodel ktx

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -21,13 +21,16 @@ object KotlinCoroutines {
 }
 
 object AndroidX {
+    val lifecycleVersion = "2.2.0"
+
     val dependencies = arrayOf(
         "implementation" to "androidx.appcompat:appcompat:1.1.0",
         "implementation" to "androidx.recyclerview:recyclerview:1.1.0",
         "implementation" to "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0",
         "implementation" to "androidx.constraintlayout:constraintlayout:1.1.3",
         "implementation" to "androidx.core:core-ktx:1.2.0",
-        "implementation" to "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
+        "implementation" to "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion",
+        "implementation" to "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"
     )
 }
 
