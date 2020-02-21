b0f337292f9429128622070a89049b9230737e8d
krzysiek zgondek <krzysiek.zgondek@gmail.com>
2020-02-21 06:17:30 +0100

    #domain models are now treated as unsafe (all nullable)

--- domain/src/main/kotlin/com/source/countries/model/Country.kt
+++ domain/src/main/kotlin/com/source/countries/model/Country.kt
@@ -1,14 +1,9 @@
 package com.source.countries.model
 
-/**
- * @project Countries
- * @author SourceOne on 21.02.2020
- */
-
 data class Country(
-    val name: String,
-    val topLevelDomain: List<String>,
-    val callingCodes: List<String>,
-    val currencies: List<Currency>
+    val name: String?,
+    val topLevelDomain: List<String>?,
+    val callingCodes: List<String>?,
+    val currencies: List<Currency>?
 )
 
--- domain/src/main/kotlin/com/source/countries/model/Currency.kt
+++ domain/src/main/kotlin/com/source/countries/model/Currency.kt
@@ -1,7 +1,7 @@
 package com.source.countries.model
 
 data class Currency(
-    val code: String,
-    val name: String,
-    val symbol: String
+    val code: String?,
+    val name: String?,
+    val symbol: String?
 )
