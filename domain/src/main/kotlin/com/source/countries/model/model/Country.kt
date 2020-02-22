00bd45d601b73c6454cb71f50391d5e54c65a233
krzysiek zgondek <krzysiek.zgondek@gmail.com>
2020-02-22 22:04:04 +0100

    #packed by layer by feature

--- domain/src/main/kotlin/com/source/countries/model/model/Country.kt
+++
@@ -1,8 +0,0 @@
-package com.source.countries.model.model
-
-data class Country(
-    val name: String?,
-    val topLevelDomain: List<String>?,
-    val callingCodes: List<String>?,
-    val currencies: List<Currency>?
-)
--- domain/src/main/kotlin/com/source/countries/model/model/Currency.kt
+++
@@ -1,7 +0,0 @@
-package com.source.countries.model.model
-
-data class Currency(
-    val code: String?,
-    val name: String?,
-    val symbol: String?
-)
